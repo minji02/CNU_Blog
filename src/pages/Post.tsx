b068f3e72dd961413f9aa108fcc276260c017800
Minji <alswl071560@naver.com>
2024-05-29 01:29:22 +0900

    Post 수정

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,4 +1,4 @@
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 import NotFound from '../components/NotFound';
 import Tag from '../components/Tag';
